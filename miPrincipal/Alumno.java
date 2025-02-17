03a41265028e93bedbab9f6bfc9de0dadeeaa7c5
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-02-17 16:52:44 +0000

    clase Persona y Alumno

---
+++ miPrincipal/Alumno.java
@@ -0,0 +1,28 @@
+package miPrincipal;
+public class Alumno extends Persona{
+    //Atributos
+    private String curso;
+    private int notas[];
+    
+    //Métodos
+    //Constructores
+    //Vacios
+    public Alumno(){
+
+    }
+    //lleno
+    public Alumno(String nombre, int edad, String dni, String curso){
+        super(nombre, edad, dni); //invoca al constructor de la clase base (Persona)
+        this.curso = curso;
+        this.notas = new int[5]; //inicializando un arreglo de nombre notas con 5 elementos y como int es cero
+
+    }
+    //Personalizados
+    public void estudiar(){
+        System.out.println("Soy una alumno y estoy estudiando");
+    }
+
+
+
+
+}
---
+++ miPrincipal/Persona.java
@@ -0,0 +1,25 @@
+package miPrincipal;
+public class Persona{
+    //Atributos
+    private String nombre;
+    private int edad;
+    private String dni;
+    
+    //Métodos
+    //Constructores
+    public Persona(){
+
+    }
+    //Vacio
+    //Lleno
+    public Persona(String nombre, int edad, String dni){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+    //Personalizados
+    public void saludar(){
+        System.out.println("Hola, soy una Persona");
+    }
+
+}
